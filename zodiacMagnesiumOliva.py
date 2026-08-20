2075b7ac7051b61f8de7f26db4dbc5045fcd9d53
mjoliva-sudo <mjoliva@brc.pshs.edu.ph>
2026-08-20 23:13:44 +0800

    Create zodiacMagnesiumOliva.py

---
+++ zodiacMagnesiumOliva.py
@@ -0,0 +1,10 @@
+Chinese_Zodiac = ["Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)", "Dragon (龙 / Lóng)", 
+                  "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)",
+                    "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
+
+year = int(input("Enter your birth year: "))
+zodiac = (year - 4) % 12 
+if year < 1900:
+    print("Invalid Year, it should not be earlier than 1900")
+else:
+    print("Your Chinese Zodiac sign is:", Chinese_Zodiac[zodiac])
